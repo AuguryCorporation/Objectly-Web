cc0076f93c09432bb958f2b0ec31c6e6c8383097
David L Norris <dave@webaugur.com>
2015-07-21 10:57:31 -0400

    Update config.js

--- public/app/config.js
+++ public/app/config.js
@@ -1,7 +1,7 @@
 window.config = {
-	role: 'arn:aws:iam::320627489957:role/WIF',
-	clientId: 'amzn1.application-oa2-client.3fe13246711f4bc5b5b2a237e5531533',
+	role: 'arn:aws:iam::23745798273592:role/yourrole',
+	clientId: 'amzn1.application-oa2-client.2387598723598908fe9ca9458',
   	region: 'us-east-1',	
 	dbTable: 'obj-browser-stats',
-	identityPoolId:'us-east-1:7399d4c6-6dd0-4c77-9686-b82c00458e79'
+	identityPoolId:'us-east-1:9823589-8978-7482-abcd-8489904994'
 };
